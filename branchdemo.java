796c61d1ae2788e149d96b5d92417ba9ac2b324d
Dibyansh Yadav <dibyanshyadav@Dibyanshs-MacBook-Pro.local>
2019-07-18 17:16:36 +0530

    a few output statements were added

---
+++ branchdemo.java
@@ -0,0 +1,14 @@
+
+public class branchdemo {
+
+
+    public static void main(String [] args) {
+
+        System.out.println("Hello world");
+        System.out.println("Namastey Prithvi");
+        System.out.println("wubba lubbss");
+        System.out.println("gg");
+
+
+    }
+}
